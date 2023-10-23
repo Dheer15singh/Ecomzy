bc5eefc743cba3d2b9893be5cd4c805a650efb62
Dheer Singh <96051806+Dheer15singh@users.noreply.github.com>
2023-10-24 00:54:09 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
       <nav className="flex justify-between items-center h-20 max-w-6xl mx-auto bg-slate-900">
         <NavLink to="/">
           <div className="ml-5">
-          <img src="../logo.png" className="h-14"/>
+          <img src="../logo.png" alt="logo" className="h-14"/>
           </div>
         </NavLink>
 
